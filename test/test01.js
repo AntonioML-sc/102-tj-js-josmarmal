55ec21f765708e0b59bc60a28fc191194eaea710
“AntonioML-sc” <antoniomoralesledesma@gmail.com>
2022-07-08 14:43:25 +0200

    Seccond attempt: 3 test passed

--- test/test01.js
+++ test/test01.js
@@ -1,5 +1,33 @@
 function numPrimo(input) {
- return -1;
+
+    const esPrimo = (num) => {
+        if (num < 2) {
+            return false;
+        } else {
+            for (let i = 2; i < num/2; i++) {
+                if (num % i == 0) {
+                    return false;
+                }                
+            }
+            return true;
+        }
+    }
+
+    const primos = (num) => {
+        let arr = [];
+        for (let i = 2; i <= num; i++) {
+            if (esPrimo(i)) {
+                arr.push(i);
+            }            
+        }
+        return arr
+    }
+
+    if (input > 1) {
+        return primos(input)
+    } else {
+        return -1
+    }
 }
 
 module.exports = numPrimo;
